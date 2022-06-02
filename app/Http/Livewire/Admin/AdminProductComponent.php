a2eb7efaf45f9878b3ec81f1b45de603e9199430
unknown <barion224@gmail.com>
2022-06-02 09:03:56 +0300

    delete product

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -8,6 +8,13 @@ use Livewire\WithPagination;
 
 class AdminProductComponent extends Component
 {
+    public function delete_product($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        session()->flash('message_success', 'Product has been deleted successfully!');
+    }   
+
     use WithPagination;
     public function render()
     {
--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -23,6 +23,11 @@
                         </div>
                     </div>
                     <div class="panel-body">
+                        @if (Session::has('message_success'))
+                            <div class="alert alert-success" role="alert">
+                                {{ Session::get('message_success') }}
+                            </div>
+                        @endif
                         <table class="table table-striped">
                             <thead>
                                 <tr>
@@ -48,6 +53,7 @@
                                         <td>{{ $product->created_at }}</td>
                                         <td>
                                             <a href="{{ route('admin.product.edit', ['product_slug' => $product->slug]) }}"><i class="fa fa-edit fa-2x"></i></a>
+                                            <a href="" wire:click.prevent="delete_product({{ $product->id }})" style="margin-left: 10px;"><i class="fa fa-times fa-2x text-danger"></i></a>
                                         </td>
                                     </tr>
                                 @endforeach
